775b0e82c2bc147b7e4e561aab3f32661e9b6e45
kwanlok.ng <kwanlok.ng@junyiacademy.org>
2023-01-22 15:40:32 +0800

    fix online map interval bugs.

--- src/containers/OnlineMap/index.tsx
+++ src/containers/OnlineMap/index.tsx
@@ -21,7 +21,6 @@ import onlineMapMarkerData from "../../pageData/onlineMapMarkerData";
 
 import icon from "leaflet/dist/images/marker-icon.png";
 import iconShadow from "leaflet/dist/images/marker-shadow.png";
-import historyIcon from "../../image/history.svg";
 import copyIcon from "../../image/copy.svg";
 
 let DefaultIcon = L.icon({
@@ -45,8 +44,8 @@ const OnlineMap: React.FC<OnlineMapProps> = ({}) => {
   const lng = searchParams.get("lng");
   const { t } = useTranslation();
   const [displayPosition, setdisplayPosition] = useState<LatLngExpression>([
-    lat ? Number(lat) : 56.08247765465308,
-    lng ? Number(lng) : -125.20586013793947,
+    lat ? Number(lat) : onlineMapMarkerData.onlineMapMarkerData[0].position[0],
+    lng ? Number(lng) : onlineMapMarkerData.onlineMapMarkerData[0].position[1],
   ]);
   const initZoom = 4;
   const [map, setMap] = useState<L.Map | null>(null);
@@ -62,6 +61,7 @@ const OnlineMap: React.FC<OnlineMapProps> = ({}) => {
     if (year) {
       setMapYear(year);
     }
+
     if (lat && lng) {
       setdisplayPosition([Number(lat), Number(lng)]);
     }
@@ -73,6 +73,7 @@ const OnlineMap: React.FC<OnlineMapProps> = ({}) => {
         history.push(`/online-map/${mapYear}?lat=${lat}&lng=${lng}`);
         setdisplayPosition([Number(lat), Number(lng)]);
       }
+
       setReloadMap(false);
     }
   }, [reloadMap]);
@@ -82,18 +83,24 @@ const OnlineMap: React.FC<OnlineMapProps> = ({}) => {
       if (isCopyPosition) {
         setIsCopyPosition(false);
       }
+
       setZoom(map.getZoom());
       setPosition(map.getCenter());
-      history.push(
-        `/online-map/${mapYear}?lat=${position?.lat}&lng=${position?.lng}`
-      );
+      const lat = position?.lat || displayPosition[0];
+      const lng = position?.lng || displayPosition[1];
+      history.push(`/online-map/${mapYear}?lat=${lat}&lng=${lng}`);
     }, [map]);
+
     useEffect(() => {
-      map.on("move", onMove);
+      const interval = setInterval(() => {
+        map.on("move", onMove);
+      }, 1000);
       return () => {
         map.off("move", onMove);
+        clearInterval(interval);
       };
     }, [map, onMove]);
+
     // console.log("zoom: " + zoom + " " + sourceZoom, map.getBounds(), position);
     return null;
   };
@@ -132,10 +139,39 @@ const OnlineMap: React.FC<OnlineMapProps> = ({}) => {
           <ul className={styles.Top__nav__list}>
             <li className={styles.Top__nav_col__right}>
               <div className={styles.Top__nav__right_title}>
-                <img
-                  src={historyIcon}
+                <svg
                   className={styles.Top__nav__right_title_icon}
-                />
+                  height="21px"
+                  version="1.1"
+                  viewBox="0 0 20 21"
+                  width="20px"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <g
+                    fill="none"
+                    fill-rule="evenodd"
+                    id="Page-1"
+                    stroke="none"
+                    stroke-width="1"
+                  >
+                    <g
+                      className={styles.Top__nav__right_title_icon}
+                      id="Core"
+                      opacity="0.9"
+                      transform="translate(-464.000000, -254.000000)"
+                    >
+                      <g
+                        id="history"
+                        transform="translate(464.000000, 254.500000)"
+                      >
+                        <path
+                          d="M10.5,0 C7,0 3.9,1.9 2.3,4.8 L0,2.5 L0,9 L6.5,9 L3.7,6.2 C5,3.7 7.5,2 10.5,2 C14.6,2 18,5.4 18,9.5 C18,13.6 14.6,17 10.5,17 C7.2,17 4.5,14.9 3.4,12 L1.3,12 C2.4,16 6.1,19 10.5,19 C15.8,19 20,14.7 20,9.5 C20,4.3 15.7,0 10.5,0 L10.5,0 Z M9,5 L9,10.1 L13.7,12.9 L14.5,11.6 L10.5,9.2 L10.5,5 L9,5 L9,5 Z"
+                          id="Shape"
+                        />
+                      </g>
+                    </g>
+                  </g>
+                </svg>
                 <span className={styles.Top__nav__right_title_content}>
                   {mapYear}
                 </span>
@@ -167,7 +203,21 @@ const OnlineMap: React.FC<OnlineMapProps> = ({}) => {
           }`}
           onClick={handleClickCopy}
         >
-          <img src={copyIcon} className={styles.onlineMap_copy_icon} />
+          <svg
+            className={styles.onlineMap_copy_icon}
+            xmlns="http://www.w3.org/2000/svg"
+            shape-rendering="geometricPrecision"
+            text-rendering="geometricPrecision"
+            image-rendering="optimizeQuality"
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            viewBox="0 0 469 511.53"
+          >
+            <path
+              fill-rule="nonzero"
+              d="M143.57 91.42h273.27c28.7 0 52.16 23.46 52.16 52.16v315.79c0 28.57-23.58 52.16-52.16 52.16H143.57c-28.69 0-52.15-23.47-52.15-52.16V143.58c0-28.72 23.44-52.16 52.15-52.16zm122.42 169.95c-9.85 13.65-30.59-1.26-20.8-14.94l18.33-25.47a59.675 59.675 0 0 1 17.1-15.96 60.646 60.646 0 0 1 22.02-8.22c16.4-2.67 32.32 1.53 44.79 10.49 12.47 8.98 21.51 22.72 24.19 39.12a60.594 60.594 0 0 1-.79 23.49 59.474 59.474 0 0 1-9.68 21.29l-18.32 25.47c-9.83 13.67-30.61-1.28-20.77-14.95l18.3-25.46c2.71-3.76 4.55-7.92 5.55-12.2 1.04-4.45 1.17-9.06.45-13.51-1.55-9.47-6.73-17.37-13.86-22.5-7.14-5.14-16.28-7.53-25.73-5.98-4.45.73-8.77 2.32-12.67 4.72a34.15 34.15 0 0 0-9.8 9.14l-18.31 25.47zm21.12 6.53c9.9-13.61 30.51 1.27 20.71 14.95l-34.04 51.43c-9.84 13.58-30.49-1.29-20.72-14.94l34.05-51.44zm6.99 74.15c9.85-13.67 30.61 1.28 20.78 14.95l-17.97 24.98c-4.74 6.58-10.59 11.94-17.11 15.96a60.398 60.398 0 0 1-22.02 8.22c-16.4 2.67-32.31-1.53-44.78-10.49-12.47-8.97-21.51-22.72-24.19-39.12a60.45 60.45 0 0 1 .78-23.46 59.833 59.833 0 0 1 9.69-21.27l18.01-25.09c9.87-13.59 30.54 1.35 20.75 14.99l-17.98 24.99a33.93 33.93 0 0 0-5.56 12.19 34.893 34.893 0 0 0-.43 13.5l.01.07c1.54 9.43 6.71 17.32 13.84 22.44 7.13 5.13 16.24 7.53 25.69 6l.07-.02c4.44-.73 8.76-2.32 12.63-4.71 3.74-2.3 7.1-5.37 9.81-9.14l17.98-24.99zm-257.52 8.77c0 10.1-8.19 18.29-18.29 18.29S0 360.92 0 350.82V52.16C0 23.44 23.44 0 52.16 0h273.26c10.1 0 18.29 8.19 18.29 18.29s-8.19 18.29-18.29 18.29H52.16c-8.54 0-15.58 7.04-15.58 15.58v298.66zM416.84 128H143.57c-8.53 0-15.57 7.04-15.57 15.58v315.79c0 8.52 7.06 15.58 15.57 15.58h273.27c8.59 0 15.58-6.99 15.58-15.58V143.58c0-8.52-7.06-15.58-15.58-15.58z"
+            />
+          </svg>
         </div>
       </div>
 
--- src/pageData/onlineMapMarkerData.tsx
+++ src/pageData/onlineMapMarkerData.tsx
@@ -10,7 +10,7 @@ const onlineMapMarkerData: IOnlineMapMarkerData = {
   onlineMapMarkerData: [
     {
       title: "online_map_spwan_point",
-      position: [56.08247765465308, -125.20586013793947],
+      position: [56.58247765465308, -58.00131225585938],
     },
   ],
 };
