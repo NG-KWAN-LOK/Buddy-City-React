0f8cad9d4be3ffad6af1206a70edd09eac2e2415
NG-KWAN-LOK <t106590052@ntut.org.tw>
2023-09-29 05:20:09 +0800

    New: to do and activities page text.

---
+++ src/pageData/activityPageData.tsx
@@ -0,0 +1,321 @@
+import { GeneralCategoryData } from "../interface/GeneralPage";
+import BUDDY_CITY_SPORT_CENTER from "../image/photo/generalpage/sport/buddy_city_sport_center.jpg";
+import WATER_CUBE from "../image/photo/generalpage/sport/water_cube.jpg";
+import BUDDY_ROAD_BASKETBALL_COURT from "../image/photo/generalpage/sport/buddy_road_besketball_court.jpg";
+import TSING_YI_SPORT_CENTER from "../image/photo/generalpage/sport/tsing_yi_sport_center.jpg";
+import BUDDY_SPORT_GROUND from "../image/photo/generalpage/sport/buddy_sport_ground.jpg";
+import WISHING_TREE from "../image/photo/generalpage/flower/wishing_tree.jpg";
+import BUDDY_CENTRAL_PARK from "../image/photo/generalpage/flower/buddy_central_park.jpg";
+import MEGUROGAWA from "../image/photo/generalpage/flower/megurogawa.jpg";
+import TSING_YI_PARK from "../image/photo/generalpage/flower/tsing_yi_park.jpg";
+import MUSIC_BOX from "../image/photo/generalpage/music/music_box.jpg";
+import BUDDY_CINEMA from "../image/photo/generalpage/music/buddy_cinema.jpg";
+import KOWLOON_CANTON_CLOCK_TOWER from "../image/photo/generalpage/monument/kowloon_canton_clock_tower.jpg";
+import TIME_HOURGLASS from "../image/photo/generalpage/monument/time_hourglass.jpg";
+import OSAKA_CASTLE from "../image/photo/generalpage/monument/osaka_castle.jpg";
+import BUDDY_CITY_GOVERNMENT_OFFICE from "../image/photo/generalpage/monument/buddy_city_government_office.jpg";
+import HOKANJI from "../image/photo/generalpage/monument/hokanji.jpg";
+import KIYOMIZU_DERA from "../image/photo/generalpage/monument/kiyomizu_dera.jpg";
+
+export const activityPageData: GeneralCategoryData = {
+  sport: {
+    pageTitle: "discover_content_atoppadding_sport",
+    pageSubtitle: "discover_content_apadding_sport_sub",
+    contentName: "sport",
+    contentData: [
+      {
+        contentType: "title",
+        contentText: "active_sport_buddy_city_sport_center",
+      },
+      {
+        contentType: "image",
+        src: BUDDY_CITY_SPORT_CENTER,
+        style: {
+          "max-width": 600,
+          "max-height": 300,
+        },
+      },
+      {
+        contentType: "text",
+        contentText: "active_sport_buddy_city_sport_center_content",
+      },
+      {
+        contentType: "title",
+        contentText: "active_water_cube",
+      },
+      {
+        contentType: "image",
+        src: WATER_CUBE,
+        style: {
+          "max-width": 600,
+          "max-height": 300,
+        },
+      },
+      {
+        contentType: "text",
+        contentText: "active_water_cube_content",
+      },
+      {
+        contentType: "title",
+        contentText: "active_buddy_road_besketball_court",
+      },
+      {
+        contentType: "image",
+        src: BUDDY_ROAD_BASKETBALL_COURT,
+        style: {
+          "max-width": 600,
+          "max-height": 300,
+        },
+      },
+      {
+        contentType: "text",
+        contentText: "active_buddy_road_besketball_court_content",
+      },
+      {
+        contentType: "title",
+        contentText: "active_buddy_sport_ground",
+      },
+      {
+        contentType: "image",
+        src: BUDDY_SPORT_GROUND,
+        style: {
+          "max-width": 600,
+          "max-height": 300,
+        },
+      },
+      {
+        contentType: "text",
+        contentText: "active_buddy_sport_ground_content",
+      },
+      {
+        contentType: "title",
+        contentText: "active_tsing_yi_sport_center",
+      },
+      {
+        contentType: "image",
+        src: TSING_YI_SPORT_CENTER,
+        style: {
+          "max-width": 600,
+          "max-height": 300,
+        },
+      },
+      {
+        contentType: "text",
+        contentText: "active_tsing_yi_sport_center_content",
+      },
+    ],
+  },
+  flower: {
+    pageTitle: "discover_content_atoppadding_flower",
+    pageSubtitle: "discover_content_apadding_flower",
+    contentName: "flower",
+    contentData: [
+      {
+        contentType: "title",
+        contentText: "flower_whishing_tree",
+      },
+      {
+        contentType: "image",
+        src: WISHING_TREE,
+        style: {
+          "max-width": 600,
+          "max-height": 300,
+        },
+      },
+      {
+        contentType: "text",
+        contentText: "flower_whishing_tree_content",
+      },
+      {
+        contentType: "title",
+        contentText: "flower_buddy_central_park",
+      },
+      {
+        contentType: "image",
+        src: BUDDY_CENTRAL_PARK,
+        style: {
+          "max-width": 600,
+          "max-height": 300,
+        },
+      },
+      {
+        contentType: "text",
+        contentText: "flower_buddy_central_park_content",
+      },
+      {
+        contentType: "title",
+        contentText: "flower_megurogawa",
+      },
+      {
+        contentType: "image",
+        src: MEGUROGAWA,
+        style: {
+          "max-width": 600,
+          "max-height": 300,
+        },
+      },
+      {
+        contentType: "text",
+        contentText: "flower_megurogawa_content",
+      },
+      {
+        contentType: "title",
+        contentText: "flower_tsing_yi_park",
+      },
+      {
+        contentType: "image",
+        src: TSING_YI_PARK,
+        style: {
+          "max-width": 600,
+          "max-height": 300,
+        },
+      },
+      {
+        contentType: "text",
+        contentText: "flower_tsing_yi_park_content",
+      },
+    ],
+  },
+  music: {
+    pageTitle: "discover_content_atoppadding_music",
+    pageSubtitle: "discover_content_apadding_music",
+    contentName: "music",
+    contentData: [
+      {
+        contentType: "title",
+        contentText: "music_music_box",
+      },
+      {
+        contentType: "image",
+        src: MUSIC_BOX,
+        style: {
+          "max-width": 600,
+          "max-height": 300,
+        },
+      },
+      {
+        contentType: "text",
+        contentText: "music_music_box_content",
+      },
+      {
+        contentType: "title",
+        contentText: "music_buddy_cinema",
+      },
+      {
+        contentType: "image",
+        src: BUDDY_CINEMA,
+        style: {
+          "max-width": 600,
+          "max-height": 300,
+        },
+      },
+      {
+        contentType: "text",
+        contentText: "music_buddy_cinema_content",
+      },
+    ],
+  },
+  monument: {
+    pageTitle: "discover_content_atoppadding_monument",
+    pageSubtitle: "discover_content_apadding_monument",
+    contentName: "monument",
+    contentData: [
+      {
+        contentType: "title",
+        contentText: "monument_kowloon_canton_clock_tower",
+      },
+      {
+        contentType: "image",
+        src: KOWLOON_CANTON_CLOCK_TOWER,
+        style: {
+          "max-width": 600,
+          "max-height": 300,
+        },
+      },
+      {
+        contentType: "text",
+        contentText: "monument_kowloon_canton_clock_tower_content",
+      },
+      {
+        contentType: "title",
+        contentText: "monument_time_hourglass",
+      },
+      {
+        contentType: "image",
+        src: TIME_HOURGLASS,
+        style: {
+          "max-width": 600,
+          "max-height": 300,
+        },
+      },
+      {
+        contentType: "text",
+        contentText: "monument_time_hourglass_content",
+      },
+      {
+        contentType: "title",
+        contentText: "monument_osaka_castle",
+      },
+      {
+        contentType: "image",
+        src: OSAKA_CASTLE,
+        style: {
+          "max-width": 600,
+          "max-height": 300,
+        },
+      },
+      {
+        contentType: "text",
+        contentText: "monument_osaka_castle_content",
+      },
+      {
+        contentType: "title",
+        contentText: "monument_buddy_city_government_office",
+      },
+      {
+        contentType: "image",
+        src: BUDDY_CITY_GOVERNMENT_OFFICE,
+        style: {
+          "max-width": 600,
+          "max-height": 300,
+        },
+      },
+      {
+        contentType: "text",
+        contentText: "monument_buddy_city_government_office_content",
+      },
+      {
+        contentType: "title",
+        contentText: "monument_hokanji",
+      },
+      {
+        contentType: "image",
+        src: HOKANJI,
+        style: {
+          "max-width": 600,
+          "max-height": 300,
+        },
+      },
+      {
+        contentType: "text",
+        contentText: "monument_hokanji_content",
+      },
+      {
+        contentType: "title",
+        contentText: "monument_kiyomizu_dera",
+      },
+      {
+        contentType: "image",
+        src: KIYOMIZU_DERA,
+        style: {
+          "max-width": 600,
+          "max-height": 300,
+        },
+      },
+      {
+        contentType: "text",
+        contentText: "monument_kiyomizu_dera_content",
+      },
+    ],
+  },
+};
---
+++ src/pageData/toDoPageData.tsx
@@ -0,0 +1,307 @@
+import { GeneralCategoryData } from "../interface/GeneralPage";
+import SUPER_RESTAURANT from "../image/photo/generalpage/enjoy/super_restaurant.jpg";
+import VA_11_HALL_A from "../image/photo/generalpage/enjoy/va_11_hall_a.jpg";
+import HOT_SPRING_CENTER from "../image/photo/generalpage/enjoy/hot_spring_center.jpg";
+import TOKYO_TOWER from "../image/photo/generalpage/enjoy/tokyo_tower.jpg";
+import PALETTE_TOWN from "../image/photo/generalpage/enjoy/palette_town.jpg";
+import PARADISE from "../image/photo/generalpage/enjoy/paradise.jpg";
+import UNDERGROUND_STREET from "../image/photo/generalpage/enjoy/underground_street.jpg";
+import DOTONBORI from "../image/photo/generalpage/enjoy/dotonbori.jpg";
+import HEP_FIVE from "../image/photo/generalpage/enjoy/hep_five.jpg";
+import SHA_TIN from "../image/photo/generalpage/play_country/sha_tin.jpg";
+import TOKYO from "../image/photo/generalpage/play_country/tokyo.jpg";
+import CAMPUS from "../image/photo/generalpage/play_country/campus.jpg";
+import HONGKONG from "../image/photo/generalpage/play_country/hongkong.jpg";
+import TSING_YI from "../image/photo/generalpage/play_country/tsing_yi.jpg";
+import OSAKA from "../image/photo/generalpage/play_country/osaka.jpg";
+import TAIPEI from "../image/photo/generalpage/play_country/taipei.jpg";
+import KYOTO from "../image/photo/generalpage/play_country/kyoto.jpg";
+
+export const toDoPageDataData: GeneralCategoryData = {
+  enjoy: {
+    pageTitle: "discover_content_atoppadding_enjoy",
+    pageSubtitle: "discover_content_apadding_enjoy",
+    contentName: "enjoy",
+    contentData: [
+      {
+        contentType: "title",
+        contentText: "enjoy_super_Restaurant",
+      },
+      {
+        contentType: "image",
+        src: SUPER_RESTAURANT,
+        style: {
+          "max-width": 600,
+          "max-height": 300,
+        },
+      },
+      {
+        contentType: "text",
+        contentText: "enjoy_super_Restaurant_content",
+      },
+      {
+        contentType: "title",
+        contentText: "enjoy_va_11_hall_a",
+      },
+      {
+        contentType: "image",
+        src: VA_11_HALL_A,
+        style: {
+          "max-width": 600,
+          "max-height": 300,
+        },
+      },
+      {
+        contentType: "text",
+        contentText: "enjoy_va_11_hall_a_content",
+      },
+      {
+        contentType: "title",
+        contentText: "enjoy_hot_spring_center",
+      },
+      {
+        contentType: "image",
+        src: HOT_SPRING_CENTER,
+        style: {
+          "max-width": 600,
+          "max-height": 300,
+        },
+      },
+      {
+        contentType: "text",
+        contentText: "enjoy_hot_spring_center_content",
+      },
+      {
+        contentType: "title",
+        contentText: "enjoy_tokyo_tower",
+      },
+      {
+        contentType: "image",
+        src: TOKYO_TOWER,
+        style: {
+          "max-width": 600,
+          "max-height": 300,
+        },
+      },
+      {
+        contentType: "text",
+        contentText: "enjoy_tokyo_tower_content",
+      },
+      {
+        contentType: "title",
+        contentText: "enjoy_palette_town",
+      },
+      {
+        contentType: "image",
+        src: PALETTE_TOWN,
+        style: {
+          "max-width": 600,
+          "max-height": 300,
+        },
+      },
+      {
+        contentType: "text",
+        contentText: "enjoy_palette_town_content",
+      },
+      {
+        contentType: "title",
+        contentText: "enjoy_paradise",
+      },
+      {
+        contentType: "image",
+        src: PARADISE,
+        style: {
+          "max-width": 600,
+          "max-height": 300,
+        },
+      },
+      {
+        contentType: "text",
+        contentText: "enjoy_paradise_content",
+      },
+      {
+        contentType: "title",
+        contentText: "enjoy_underground_street",
+      },
+      {
+        contentType: "image",
+        src: UNDERGROUND_STREET,
+        style: {
+          "max-width": 600,
+          "max-height": 300,
+        },
+      },
+      {
+        contentType: "text",
+        contentText: "enjoy_underground_street_content",
+      },
+      {
+        contentType: "title",
+        contentText: "enjoy_dotonbori",
+      },
+      {
+        contentType: "image",
+        src: DOTONBORI,
+        style: {
+          "max-width": 600,
+          "max-height": 300,
+        },
+      },
+      {
+        contentType: "text",
+        contentText: "enjoy_dotonbori_content",
+      },
+      {
+        contentType: "title",
+        contentText: "enjoy_hep_five",
+      },
+      {
+        contentType: "image",
+        src: HEP_FIVE,
+        style: {
+          "max-width": 600,
+          "max-height": 300,
+        },
+      },
+      {
+        contentType: "text",
+        contentText: "enjoy_hep_five_content",
+      },
+    ],
+  },
+  "play-country": {
+    pageTitle: "discover_content_atoppadding_playCountry",
+    pageSubtitle: "discover_content_apadding_playCountry",
+    contentName: "play_country",
+    contentData: [
+      {
+        contentType: "title",
+        contentText: "play_country_sha_tin",
+      },
+      {
+        contentType: "image",
+        src: SHA_TIN,
+        style: {
+          "max-width": 600,
+          "max-height": 300,
+        },
+      },
+      {
+        contentType: "text",
+        contentText: "play_country_sha_tin_content",
+      },
+      {
+        contentType: "title",
+        contentText: "play_country_tokyo",
+      },
+      {
+        contentType: "image",
+        src: TOKYO,
+        style: {
+          "max-width": 600,
+          "max-height": 300,
+        },
+      },
+      {
+        contentType: "text",
+        contentText: "play_country_tokyo_content",
+      },
+      {
+        contentType: "title",
+        contentText: "play_country_campus",
+      },
+      {
+        contentType: "image",
+        src: CAMPUS,
+        style: {
+          "max-width": 600,
+          "max-height": 300,
+        },
+      },
+      {
+        contentType: "text",
+        contentText: "play_country_campus_content",
+      },
+      {
+        contentType: "title",
+        contentText: "play_country_hongkong",
+      },
+      {
+        contentType: "image",
+        src: HONGKONG,
+        style: {
+          "max-width": 600,
+          "max-height": 300,
+        },
+      },
+      {
+        contentType: "text",
+        contentText: "play_country_hongkong_content",
+      },
+      {
+        contentType: "title",
+        contentText: "play_country_tsing_yi",
+      },
+      {
+        contentType: "image",
+        src: TSING_YI,
+        style: {
+          "max-width": 600,
+          "max-height": 300,
+        },
+      },
+      {
+        contentType: "text",
+        contentText: "play_country_tsing_yi_content",
+      },
+      {
+        contentType: "title",
+        contentText: "play_country_osaka",
+      },
+      {
+        contentType: "image",
+        src: OSAKA,
+        style: {
+          "max-width": 600,
+          "max-height": 300,
+        },
+      },
+      {
+        contentType: "text",
+        contentText: "play_country_osaka_content",
+      },
+      {
+        contentType: "title",
+        contentText: "play_country_taipei",
+      },
+      {
+        contentType: "image",
+        src: TAIPEI,
+        style: {
+          "max-width": 600,
+          "max-height": 300,
+        },
+      },
+      {
+        contentType: "text",
+        contentText: "play_country_taipei_content",
+      },
+      {
+        contentType: "title",
+        contentText: "play_country_kyoto",
+      },
+      {
+        contentType: "image",
+        src: KYOTO,
+        style: {
+          "max-width": 600,
+          "max-height": 300,
+        },
+      },
+      {
+        contentType: "text",
+        contentText: "play_country_kyoto_content",
+      },
+    ],
+  },
+};
