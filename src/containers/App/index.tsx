ebf67e578b9da5497ebb08685c4ec8b9c6263d1e
NG-KWAN-LOK <sambig12004@gmail.com>
2024-01-30 04:15:54 +0800

    new: install MUI.

--- src/containers/App/index.tsx
+++ src/containers/App/index.tsx
@@ -2,6 +2,8 @@ import React, { useEffect } from "react";
 import { Route, Switch, useLocation, Redirect } from "react-router-dom";
 import styles from "./style.module.scss";
 import useTheme from "../../hoc/ThemeProvider";
+import { ThemeProvider } from "@mui/material/styles";
+import { useTheme as useMuiTheme, createTheme } from "@mui/material/styles";
 
 import "../../i18n/index.js";
 import { useTranslation } from "react-i18next";
@@ -31,6 +33,13 @@ import { howToVisitPageData } from "../../pageData/howToVisitPageData";
 import OnlineMap from "../OnlineMap";
 
 function App() {
+  const theme = useMuiTheme();
+  const darkTheme = createTheme({
+    palette: {
+      mode: "dark",
+    },
+  });
+
   const { themeMode } = useTheme();
   const { pathname } = useLocation();
   const { t, i18n } = useTranslation();
@@ -62,93 +71,95 @@ function App() {
 
   return (
     <div className={styles.app} data-theme={themeMode}>
-      <div className={styles.PaddingTop}>　</div>
-      <div className={styles.content}>
-        <Header />
-        <Switch>
-          <Route exact path='/'>
-            <Dashboard />
-          </Route>
-          <Route path={["/resident/:username", "/resident"]}>
-            <Resident />
-          </Route>
-          <Route
-            path={[
-              "/building_list/:districtid/:buildingid",
-              "/building_list/:districtid/",
-              "/building_list",
-            ]}
-          >
-            <BuildingList />
-          </Route>
-          <Route path='/bcctb-about-us'>
-            <SimplePageContainer
-              contain={<BCCTBContainer />}
-              sideMenu={<SideMenu buttonData={bcctbAboutUsSideMenuData} />}
-            />
-          </Route>
-          <Route path='/tnc'>
-            <SimplePageContainer
-              contain={<TNCContainer />}
-              sideMenu={<SideMenu buttonData={bcctbAboutUsSideMenuData} />}
-            />
-          </Route>
-          <Route path='/page/basic/:pagename'>
-            <GeneralCategory
-              generalCategoryData={basicPageData}
-              sideMenu={<SideMenu buttonData={bcctbAboutUsSideMenuData} />}
-            />
-          </Route>
-          <Route path='/page/activity/:pagename'>
-            <GeneralCategory
-              generalCategoryData={activityPageData}
-              sideMenu={<SideMenu buttonData={bcctbAboutUsSideMenuData} />}
-            />
-          </Route>
-          <Route path='/page/how-to-visit/:pagename'>
-            <GeneralCategory
-              generalCategoryData={howToVisitPageData}
-              sideMenu={<SideMenu buttonData={bcctbAboutUsSideMenuData} />}
-            />
-          </Route>
-          <Route path='/page/to-do/:pagename'>
-            <GeneralCategory
-              generalCategoryData={toDoPageDataData}
-              sideMenu={<SideMenu buttonData={bcctbAboutUsSideMenuData} />}
-            />
-          </Route>
-          <Route path='/page/subway-info/:pagename'>
-            <SubwayInfoPageContainer
-              sideMenu={<SideMenu buttonData={bcctbAboutUsSideMenuData} />}
-            />
-          </Route>
-          <Route path='/page/:pagename'>
-            <DiscoveryPage />
-          </Route>
-          <Route
-            path={[
-              "/history/railway-history/:timeSlot",
-              "/history/railway-history",
-            ]}
-          >
-            <RailwayHistoryPageContainer
-              sideMenu={<SideMenu buttonData={bcctbAboutUsSideMenuData} />}
-            />
-          </Route>
-          <Route path='/railway-route-map'>
-            <RailwayRouteMapPageContainer
-              sideMenu={<SideMenu buttonData={bcctbAboutUsSideMenuData} />}
-            />
-          </Route>
-          <Route path={["/online-map/:year", "/online-map"]}>
-            <OnlineMap />
-          </Route>
-          <Route path={["/page", "/subway-info", "/history"]}>
-            <Redirect to='/' />
-          </Route>
-        </Switch>
-        <Footer />
-      </div>
+      <ThemeProvider theme={themeMode === "dark" ? darkTheme : theme}>
+        <div className={styles.PaddingTop}>　</div>
+        <div className={styles.content}>
+          <Header />
+          <Switch>
+            <Route exact path='/'>
+              <Dashboard />
+            </Route>
+            <Route path={["/resident/:username", "/resident"]}>
+              <Resident />
+            </Route>
+            <Route
+              path={[
+                "/building_list/:districtid/:buildingid",
+                "/building_list/:districtid/",
+                "/building_list",
+              ]}
+            >
+              <BuildingList />
+            </Route>
+            <Route path='/bcctb-about-us'>
+              <SimplePageContainer
+                contain={<BCCTBContainer />}
+                sideMenu={<SideMenu buttonData={bcctbAboutUsSideMenuData} />}
+              />
+            </Route>
+            <Route path='/tnc'>
+              <SimplePageContainer
+                contain={<TNCContainer />}
+                sideMenu={<SideMenu buttonData={bcctbAboutUsSideMenuData} />}
+              />
+            </Route>
+            <Route path='/page/basic/:pagename'>
+              <GeneralCategory
+                generalCategoryData={basicPageData}
+                sideMenu={<SideMenu buttonData={bcctbAboutUsSideMenuData} />}
+              />
+            </Route>
+            <Route path='/page/activity/:pagename'>
+              <GeneralCategory
+                generalCategoryData={activityPageData}
+                sideMenu={<SideMenu buttonData={bcctbAboutUsSideMenuData} />}
+              />
+            </Route>
+            <Route path='/page/how-to-visit/:pagename'>
+              <GeneralCategory
+                generalCategoryData={howToVisitPageData}
+                sideMenu={<SideMenu buttonData={bcctbAboutUsSideMenuData} />}
+              />
+            </Route>
+            <Route path='/page/to-do/:pagename'>
+              <GeneralCategory
+                generalCategoryData={toDoPageDataData}
+                sideMenu={<SideMenu buttonData={bcctbAboutUsSideMenuData} />}
+              />
+            </Route>
+            <Route path='/page/subway-info/:pagename'>
+              <SubwayInfoPageContainer
+                sideMenu={<SideMenu buttonData={bcctbAboutUsSideMenuData} />}
+              />
+            </Route>
+            <Route path='/page/:pagename'>
+              <DiscoveryPage />
+            </Route>
+            <Route
+              path={[
+                "/history/railway-history/:timeSlot",
+                "/history/railway-history",
+              ]}
+            >
+              <RailwayHistoryPageContainer
+                sideMenu={<SideMenu buttonData={bcctbAboutUsSideMenuData} />}
+              />
+            </Route>
+            <Route path='/railway-route-map'>
+              <RailwayRouteMapPageContainer
+                sideMenu={<SideMenu buttonData={bcctbAboutUsSideMenuData} />}
+              />
+            </Route>
+            <Route path={["/online-map/:year", "/online-map"]}>
+              <OnlineMap />
+            </Route>
+            <Route path={["/page", "/subway-info", "/history"]}>
+              <Redirect to='/' />
+            </Route>
+          </Switch>
+          <Footer />
+        </div>
+      </ThemeProvider>
     </div>
   );
 }
